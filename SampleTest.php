39371a5fb899ef7bc5ea4df1aac338e1e087120f
kimura_yuuto <halu.k1126@gmail.com>
2018-01-23 08:52:52 +0900

    test

---
+++ SampleTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once('./Sample.php');
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testHelloworld()
+    {
+        $Sample = new Sample();
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
+    }
+}
